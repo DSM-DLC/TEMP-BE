de3693ee48518cf10b367df71c48a66b4a68ae30
csh0927 <sung060927@gmail.com>
2023-09-15 17:04:07 +0900

    Init: Project initial settings

---
+++ src/test/java/com/example/tempbe/TempBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tempbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
