903867935a08d91387c46f490cde640525d23146
KMS06 <jjm336949@dsm.hs.kr>
2023-10-12 10:10:26 +0900

    Fix: entity 수정
    UUID는 identity 안되고 binary(16) 해줘야 한다고 함

--- src/main/java/com/example/tempbe/global/entity/BaseIdEntity.java
+++ src/main/java/com/example/tempbe/global/entity/BaseIdEntity.java
@@ -1,6 +1,7 @@
 package com.example.tempbe.global.entity;
 
 import lombok.Getter;
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.*;
 import java.util.UUID;
@@ -10,6 +11,8 @@ import java.util.UUID;
 @EntityListeners(AuditingEntityListener.class)
 public class BaseIdEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    UUID id;
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
 }
