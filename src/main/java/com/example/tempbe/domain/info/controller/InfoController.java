02a3d2c5f65cbf9f247de77940641d19d32eb5b5
KMS06 <jjm336949@dsm.hs.kr>
2023-10-25 19:58:49 +0900

    Revert "Chore: (#27) InfoController 수정"
    This reverts commit aa8a602008e5fde1739ba1ad6976316ebf5cebfe.

--- src/main/java/com/example/tempbe/domain/info/controller/InfoController.java
+++ src/main/java/com/example/tempbe/domain/info/controller/InfoController.java
@@ -1,9 +1,7 @@
 package com.example.tempbe.domain.info.controller;
 
-import com.example.tempbe.domain.info.controller.request.InfoUpdateRequest;
 import com.example.tempbe.domain.info.controller.request.InfoUploadRequest;
 import com.example.tempbe.domain.info.service.InfoUploadService;
-import com.example.tempbe.domain.info.service.InfoUpdateService
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,17 +13,10 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class InfoController {
     private final InfoUploadService infoUploadService;
-    private final InfoUpdateService infoUpdateService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/upload")
     public void upload(@RequestBody @Valid InfoUploadRequest request){
         infoUploadService.execute(request);
-    }
-
-    @ResponseStatus(HttpStatus.OK)
-    @PutMapping("/update")
-    public void update(@RequestBody @Valid InfoUpdateRequest request){
-        infoUpdateService.excute(request);
     }
 }
