fb312838452588a1dc5904dca27201eabc40d03e
KMS06 <jjm336949@dsm.hs.kr>
2023-10-25 22:30:31 +0900

    Chore: (#27) InfoRepository 수정

--- src/main/java/com/example/tempbe/domain/info/domain/InfoRepository.java
+++ src/main/java/com/example/tempbe/domain/info/domain/InfoRepository.java
@@ -3,8 +3,10 @@ package com.example.tempbe.domain.info.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface InfoRepository extends JpaRepository<Info, UUID> {
+    Optional<Info> findByNameAndBirthDate(String name, String birthDate);
 }
