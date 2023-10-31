0fef0b3a285fb6210620d6714b70acb0cfa03aaa
KMS06 <jjm336949@dsm.hs.kr>
2023-10-31 20:11:23 +0900

    Init: (#26) InfoController 수정
    infoFind URL 추가

--- src/main/java/com/example/tempbe/domain/info/controller/InfoController.java
+++ src/main/java/com/example/tempbe/domain/info/controller/InfoController.java
@@ -1,9 +1,12 @@
 package com.example.tempbe.domain.info.controller;
 
+import com.example.tempbe.domain.info.controller.request.InfoFindRequest;
 import com.example.tempbe.domain.info.controller.request.InfoUpdateRequest;
 import com.example.tempbe.domain.info.controller.request.InfoUploadRequest;
+import com.example.tempbe.domain.info.controller.response.InfoFindResponse;
 import com.example.tempbe.domain.info.service.InfoUpdateService;
 import com.example.tempbe.domain.info.service.InfoUploadService;
+import com.example.tempbe.domain.info.service.InfoFindService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +19,7 @@ import javax.validation.Valid;
 public class InfoController {
     private final InfoUploadService infoUploadService;
     private final InfoUpdateService infoUpdateService;
+    private final InfoFindService infoFindService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/upload")
@@ -26,5 +30,10 @@ public class InfoController {
     @PatchMapping("/update")
     public void update(@RequestBody @Valid InfoUpdateRequest request){
         infoUpdateService.execute(request);
+    }
+
+    @GetMapping("/find")
+    public InfoFindResponse find(@RequestBody @Valid InfoFindRequest request){
+        return infoFindService.execute(request);
     }
 }
