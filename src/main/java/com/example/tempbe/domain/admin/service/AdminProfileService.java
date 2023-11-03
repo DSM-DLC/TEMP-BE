76896f37284858d0e97d21873f1ba12c47250ceb
KMS06 <jjm336949@dsm.hs.kr>
2023-11-03 11:17:31 +0900

    Init: (#36) AdminProfileService 생성

---
+++ src/main/java/com/example/tempbe/domain/admin/service/AdminProfileService.java
@@ -0,0 +1,30 @@
+package com.example.tempbe.domain.admin.service;
+
+import com.example.tempbe.domain.admin.controller.response.AdminProfileResponse;
+import com.example.tempbe.domain.admin.domain.Admin;
+import com.example.tempbe.domain.admin.domain.AdminRepository;
+import com.example.tempbe.domain.admin.exception.AdminNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class AdminProfileService {
+    private final AdminRepository adminRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public AdminProfileResponse execute(){
+        String adminId = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Admin admin = adminRepository.findByAdminId(adminId)
+                .orElseThrow(() -> AdminNotFoundException.EXCEPTION);
+
+        return AdminProfileResponse.builder()
+                .adminId(admin.getAdminId())
+                .build();
+    }
+}
