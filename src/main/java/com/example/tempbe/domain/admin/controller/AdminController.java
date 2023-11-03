7d5cb178f149314f2097ffc337c63312bb06c85b
KMS06 <jjm336949@dsm.hs.kr>
2023-11-03 11:17:55 +0900

    Chore: (#36) AdminController 수정
    adminProfile URL 추가

--- src/main/java/com/example/tempbe/domain/admin/controller/AdminController.java
+++ src/main/java/com/example/tempbe/domain/admin/controller/AdminController.java
@@ -2,19 +2,15 @@ package com.example.tempbe.domain.admin.controller;
 
 import com.example.tempbe.domain.admin.controller.request.AdminLoginRequest;
 import com.example.tempbe.domain.admin.controller.request.IssueRequest;
+import com.example.tempbe.domain.admin.controller.response.AdminProfileResponse;
 import com.example.tempbe.domain.admin.service.AdminIssueService;
 import com.example.tempbe.domain.admin.service.AdminLoginService;
+import com.example.tempbe.domain.admin.service.AdminProfileService;
 import com.example.tempbe.domain.admin.service.AdminRefreshService;
 import com.example.tempbe.domain.auth.controller.response.TokenResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
@@ -26,6 +22,7 @@ public class AdminController {
     private final AdminLoginService adminLoginService;
     private final AdminRefreshService adminRefreshService;
     private final AdminIssueService adminIssueService;
+    private final AdminProfileService adminProfileService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/login")
@@ -42,5 +39,10 @@ public class AdminController {
     @PostMapping("/issue")
     public void issue(@RequestBody @Valid IssueRequest request){
         adminIssueService.execute(request);
+    }
+
+    @GetMapping("/profile")
+    public AdminProfileResponse profile(){
+        return adminProfileService.execute();
     }
 }
