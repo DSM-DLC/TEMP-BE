1315c1f64a23d9b5c2f4648ddc8e5b8164f7fc5a
KMS06 <jjm336949@dsm.hs.kr>
2023-11-07 18:19:01 +0900

    Chore: (#43) AdminController 수정

--- src/main/java/com/example/tempbe/domain/admin/controller/AdminController.java
+++ src/main/java/com/example/tempbe/domain/admin/controller/AdminController.java
@@ -1,12 +1,10 @@
 package com.example.tempbe.domain.admin.controller;
 
+import com.example.tempbe.domain.admin.controller.request.AdminChangePasswordRequest;
 import com.example.tempbe.domain.admin.controller.request.AdminLoginRequest;
 import com.example.tempbe.domain.admin.controller.request.IssueRequest;
 import com.example.tempbe.domain.admin.controller.response.AdminProfileResponse;
-import com.example.tempbe.domain.admin.service.AdminIssueService;
-import com.example.tempbe.domain.admin.service.AdminLoginService;
-import com.example.tempbe.domain.admin.service.AdminProfileService;
-import com.example.tempbe.domain.admin.service.AdminRefreshService;
+import com.example.tempbe.domain.admin.service.*;
 import com.example.tempbe.domain.auth.controller.response.TokenResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,6 +21,7 @@ public class AdminController {
     private final AdminRefreshService adminRefreshService;
     private final AdminIssueService adminIssueService;
     private final AdminProfileService adminProfileService;
+    private final AdminChangePasswordService adminChangePasswordService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/login")
@@ -44,5 +43,10 @@ public class AdminController {
     @GetMapping("/profile")
     public AdminProfileResponse profile(){
         return adminProfileService.execute();
+    }
+
+    @PatchMapping("/password")
+    public void changePassword(@RequestBody @Valid AdminChangePasswordRequest request){
+        adminChangePasswordService.execute(request);
     }
 }
