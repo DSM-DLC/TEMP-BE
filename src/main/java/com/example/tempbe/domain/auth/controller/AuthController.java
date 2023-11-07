d527a9e6c9f912d210884f8b5a87bfdadd6e8670
csh0927 <sung060927@gmail.com>
2023-11-07 21:29:30 +0900

    Chore: (#52) AuthController 수정

--- src/main/java/com/example/tempbe/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/tempbe/domain/auth/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.example.tempbe.domain.auth.controller;
 
 
-import com.example.tempbe.domain.auth.service.AuthService;
+import com.example.tempbe.domain.auth.controller.response.TokenResponse;
+import com.example.tempbe.domain.auth.service.AuthLogoutService;
+import com.example.tempbe.domain.auth.service.AuthRefreshService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +17,16 @@ import javax.validation.constraints.NotBlank;
 @RequestMapping("/auth")
 @RestController
 public class AuthController {
-    private final AuthService authService;
+    private final AuthLogoutService authLogoutService;
+    private final AuthRefreshService authRefreshService;
 
     @DeleteMapping("/logout")
-    public void logout(@RequestHeader("refresh-token") @NotBlank(message = "입력해주세요.") String refreshToken){
-        authService.logout(refreshToken);
+    public void logout(@RequestHeader("Authorization") String accessToken, @RequestHeader("refresh-token") @NotBlank(message = "입력해주세요.") String refreshToken){
+        authLogoutService.execute(accessToken, refreshToken);
+    }
+
+    @PutMapping("/refresh")
+    public TokenResponse refreshToken(@RequestHeader("refresh-token") @NotBlank(message = "입력해주세요.") String refreshToken) {
+        return authRefreshService.execute(refreshToken);
     }
 }
