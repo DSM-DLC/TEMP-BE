31e066f5f786f4d824cde525f1a6fdcc3f0ef835
csh0927 <sung060927@gmail.com>
2023-11-07 21:30:42 +0900

    Chore: (#52) AdminController 수정

--- src/main/java/com/example/tempbe/domain/admin/controller/AdminController.java
+++ src/main/java/com/example/tempbe/domain/admin/controller/AdminController.java
@@ -4,34 +4,36 @@ import com.example.tempbe.domain.admin.controller.request.AdminChangePasswordReq
 import com.example.tempbe.domain.admin.controller.request.AdminLoginRequest;
 import com.example.tempbe.domain.admin.controller.request.IssueRequest;
 import com.example.tempbe.domain.admin.controller.response.AdminProfileResponse;
-import com.example.tempbe.domain.admin.service.*;
+import com.example.tempbe.domain.admin.service.AdminChangePasswordService;
+import com.example.tempbe.domain.admin.service.AdminIssueService;
+import com.example.tempbe.domain.admin.service.AdminLoginService;
+import com.example.tempbe.domain.admin.service.AdminProfileService;
 import com.example.tempbe.domain.auth.controller.response.TokenResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
 
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 @RestController
 public class AdminController {
     private final AdminLoginService adminLoginService;
-    private final AdminRefreshService adminRefreshService;
     private final AdminIssueService adminIssueService;
     private final AdminProfileService adminProfileService;
     private final AdminChangePasswordService adminChangePasswordService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/login")
-    public TokenResponse login(@RequestBody @Valid AdminLoginRequest request){
+    public TokenResponse login(@RequestBody @Valid AdminLoginRequest request) {
         return adminLoginService.execute(request);
-    }
-
-    @PutMapping("/refresh")
-    public TokenResponse refreshToken(@RequestHeader("refresh-token") @NotBlank(message = "입력해주세요.") String refreshToken) {
-        return adminRefreshService.execute(refreshToken);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
